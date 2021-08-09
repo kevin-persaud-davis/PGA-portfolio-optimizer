d6a3eef44cb0f57f08f4dbdec376f4b8f1d96a94
Kevin Davis <kp.davis@outlook.com>
2021-08-09 16:46:04 -0400

    Initial config settings

---
+++ config/config.py
@@ -0,0 +1,22 @@
+import sys
+from pathlib import Path
+
+# Directories
+BASE_DIR = Path.home()
+ML_DIR = Path(BASE_DIR, "machine_learning")
+PROJECT_DIR = Path(ML_DIR, "projects")
+PGA_DIR = Path(PROJECT_DIR, "PGA-portfolio-optimizer")
+
+CONFIG_DIR = Path(PGA_DIR, "config")
+LOGS_DIR = Path(PGA_DIR, "logging")
+MODEL_DIR = Path(PGA_DIR, "model")
+DATA_DIR = Path(PGA_DIR, "data")
+
+PROCESSED_DATA_DIR = Path(DATA_DIR, "processed")
+RAW_DATA_DIR = Path(DATA_DIR, "raw")
+
+
+
+if __name__ == "__main__":
+    print(BASE_DIR)
+    print(RAW_DATA_DIR)
