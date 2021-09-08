e4cb788b1934966cdc90ce9493a407e97dad2d67
Kevin Davis <kp.davis@outlook.com>
2021-09-08 16:26:58 -0400

    added new directories for timeseries framework and iid framework to seperate results

--- config/config.py
+++ config/config.py
@@ -12,13 +12,16 @@ LOGS_DIR = Path(PGA_DIR, "logging")
 MODEL_DIR = Path(PGA_DIR, "model")
 DATA_DIR = Path(PGA_DIR, "data")
 
-
 RAW_DATA_DIR = Path(DATA_DIR, "raw")
 RAW_HISTORICAL_DIR = Path(RAW_DATA_DIR, "historical_player_data")
 RAW_PGA_METRICS_DIR = Path(RAW_DATA_DIR, "pgatour_metrics_data")
+
 
 PROCESSED_DATA_DIR = Path(DATA_DIR, "processed")
 
 TOURNAMENTS_DIR = Path(PROCESSED_DATA_DIR, "tournaments")
 PROCESSED_HISTORICAL_DIR = Path(PROCESSED_DATA_DIR, "historical_player_data")
+
 FEATURES_DIR = Path(PROCESSED_DATA_DIR, "features_data")
+TIMESERIES_FRAMEWORK_DIR = Path(FEATURES_DIR, "timeseries_framework")
+IID_FRAMEWORK_DIR = Path(FEATURES_DIR, "iid_framework")
