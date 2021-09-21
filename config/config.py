848cfc083db1cc4dfbe8906f436b5ca39dbcf9e5
Kevin Davis <kp.davis@outlook.com>
2021-09-21 16:07:00 -0400

    added new historical player data folders

--- config/config.py
+++ config/config.py
@@ -14,6 +14,11 @@ DATA_DIR = Path(PGA_DIR, "data")
 
 RAW_DATA_DIR = Path(DATA_DIR, "raw")
 RAW_HISTORICAL_DIR = Path(RAW_DATA_DIR, "historical_player_data")
+RAW_HISTORICAL_PRICES = Path(RAW_DATA_DIR, "historical_prices")
+PGA_SEASON_2011_2016 = Path(RAW_HISTORICAL_DIR, "pga_seasons_2011_2016")
+PGA_SEASON_2016 = Path(RAW_HISTORICAL_DIR, "season_2016")
+PGA_SEASON_2015 = Path(RAW_HISTORICAL_DIR, "season_2015")
+PGA_SEASON_2014 = Path(RAW_HISTORICAL_DIR, "season_2014")
 RAW_PGA_METRICS_DIR = Path(RAW_DATA_DIR, "pgatour_metrics_data")
 
 
---
+++ config/constants.py
@@ -0,0 +1,8 @@
+
+
+# PGATOUR METRICS
+
+# Strokes Gained: Approach The Green (SG: APP) = 02568
+
+# Strokes Gained: Tee-to-green
+
