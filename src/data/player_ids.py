3e057c3bcaffd66685c28e564c771b137fd6e1af
Kevin Davis <kp.davis@outlook.com>
2021-11-03 17:21:24 -0400

    inital commit

---
+++ src/data/player_ids.py
@@ -0,0 +1,15 @@
+import os
+import sys
+sys.path.append("c:\\Users\\kpdav\\machine_learning\\projects\\PGA-portfolio-optimizer\\config")
+import config
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
