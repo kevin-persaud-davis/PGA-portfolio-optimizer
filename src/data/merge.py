611e28cbaa0d11d30da09da32e6bbd6f5c2febf8
Kevin Davis <kp.davis@outlook.com>
2021-11-05 20:22:08 -0400

    inital commit

---
+++ src/data/merge.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+import re
+import sys
+sys.path.append("c:\\Users\\kpdav\\machine_learning\\projects\\PGA-portfolio-optimizer\\config")
+import config
+
+import pandas as pd
+pd.options.display.max_columns = 999
+import numpy as np
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
