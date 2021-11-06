565bba119ce77eca28f500481d245f2e7dc2b858
Kevin Davis <kp.davis@outlook.com>
2021-11-05 21:25:14 -0400

    added cleaning objectives for each metric

--- src/data/merge.py
+++ src/data/merge.py
@@ -9,6 +9,89 @@ import pandas as pd
 pd.options.display.max_columns = 999
 import numpy as np
 
+"""
+PGATOUR METRICS
+
+General cleaning step: add pgatour stat id into columns
+
+- Rank this week and rank last week: change from str to int
+    > replace "T"
+    > cast to int
+
+
+077 : GIR percentage - 75-100 yards
+078 :
+079 :
+- Relative to par:
+    > replace E with 0
+    > cast str to float
+
+103: Greens in regulation percentage
+- Relative to par:
+    > replace E with 0
+    > cast str to float
+- GREENS_HIT:
+    > cast str to float
+- #_HOLES:
+    > cast str to float
+
+190: GIR percentage from fairway
+- #_OF_HOLES:
+    > cast str to int
+
+199: GIR percentage from other than fairway
+
+326: GIR percentage - 200+ yards
+327:
+328:
+329:
+330:
+02330:
+- Relative to par:
+    > replace E with 0
+    > cast str to float
+
+02332: GIR percentage - 100+ yards
+- TOTAL_HOLES: remove commas
+    > replace ","
+    > cast str to int
+- RELATIVE_TO_PAR: 
+    > replace E with 0
+    > cast str to float
+
+02434: GIR pct. - fairway bunker
+- RELATIVE/PAR:
+    > replace E with 0
+    > cast str to float
+
+----------------------------------
+
+02437: Greens or fringe in regulation
+
+TOTAL_HIT:
+    > replace ","
+    > cast str to int
+TOTAL_ATTEMPTS:
+    > replace ","
+    > cast str to int
+RTP_SCORE:
+    > cast str to float
+
+
+02568: SG: approach the green (GOOD)
+02674: SG: tee-to-green (GOOD)
+02674: SG: total (GOOD)
+
+"""
+
+
+
+
+
+
+
+
+
 
 def main():
     pass
